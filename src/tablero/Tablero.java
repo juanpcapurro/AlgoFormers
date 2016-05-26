156be8dc46c3cbfd5f71ea09eaf41ae23794a05d
Juan Pablo Capurro <juanpcapurro@gmail.com>
2016-05-25 21:26:03 -0300

    hice el constructor de Tablero

--- src/tablero/Tablero.java
+++ src/tablero/Tablero.java
@@ -1,5 +1,11 @@
 package algoFormers.tablero;
 
 public class Tablero{
-	
+	private final int DIMENSION;
+	private Casillero[][] matrizCasilleros;
+
+	public Tablero(int dimension_pedida){
+		matrizCasilleros = new Casillero[dimension_pedida][dimension_pedida];
+		DIMENSION = dimension_pedida;
+	}
 }
