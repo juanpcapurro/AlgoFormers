0db60a6b33a1712a195132f1e6ffd3f284079330
MARIA FLORENCIA PRADO <mfprado@icloud.com>
2016-05-31 10:42:39 -0300

    test0i

--- test/algoFormers/tablero/colocable/BumblebeeTest.java
+++ test/algoFormers/tablero/colocable/BumblebeeTest.java
@@ -11,14 +11,14 @@ public class BumblebeeTest {
     Bumblebee mauricio = new Bumblebee();
 
     @Test
-    public void bumblebeeRecibeAtaqueYSigueConVida() {
+    public void test01BumblebeeRecibeAtaqueYSigueConVida() {
 
         mauricio.recibirAtaque(4);
         assertTrue(mauricio.estaVivo());
     }
 
     @Test
-    public void bumblebeeRecibeAtaqueYMuere() {
+    public void test02BumblebeeRecibeAtaqueYMuere() {
 
         mauricio.recibirAtaque(550);
         assertFalse(mauricio.estaVivo());
