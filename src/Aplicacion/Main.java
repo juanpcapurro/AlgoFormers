7b4f5ab0e7d8cd683557d0b8eb2a0f84f97eae8c
MARIA FLORENCIA PRADO <mfprado@icloud.com>
2016-06-09 15:05:47 -0300

    Vista

---
+++ src/Aplicacion/Main.java
@@ -0,0 +1,22 @@
+package Aplicacion;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import vista.mainApp;
+
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setResizable(false);
+        mainApp mainApp = new mainApp();
+        mainApp.start(primaryStage);
+
+    }
+
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
