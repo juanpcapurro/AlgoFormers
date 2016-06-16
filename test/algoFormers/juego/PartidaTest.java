f64c6e2b042367fe2b7b6545bf441e22738ed5a9
MARIA FLORENCIA PRADO <mfprado@icloud.com>
2016-06-16 02:05:41 -0300

    Tests modificados

--- test/algoFormers/juego/PartidaTest.java
+++ test/algoFormers/juego/PartidaTest.java
@@ -1,4 +1,12 @@
 package algoFormers.juego;
 
+import org.junit.Before;
+
 public class PartidaTest {
+    Partida partida;
+
+    @Before
+    public void Setup(){
+        partida= new Partida("Mauricio","Pablo",9);
+    }
 }
--- test/algoFormers/tablero/colocable/robots/autobot/AutobotsTest.java
+++ test/algoFormers/tablero/colocable/robots/autobot/AutobotsTest.java
@@ -2,30 +2,38 @@ package algoFormers.tablero.colocable.robots.autobot;
 
 
 import algoFormers.tablero.colocable.robots.decepticon.BoneCrusher;
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 
-public class AutobotTest{
+public class AutobotsTest{
+    Autobots autobots;
+
+    @Before
+    public void Setup(){
+        autobots= new Autobots();
+    }
     @Test
     public void test01ElAutobotTipoOptimusOcupaLugar(){
 
-        Optimus optimus = new Optimus();
+        Optimus optimus = autobots.getOptimus();
         assertTrue(optimus.ocupaLugar());
     }
 
     @Test
     public void test02ElAutobotTipoBumplebeeOcupaLugar(){
 
-        Bumblebee bumblebee = new Bumblebee();
+        Bumblebee bumblebee = autobots.getBumblebee();
         assertTrue(bumblebee.ocupaLugar());
     }
 
     @Test
     public void test03ElAutobotTipoRatchetOcupaLugar(){
 
-        Ratchet ratchet = new Ratchet();
+        Ratchet ratchet = autobots.getRatchet();
         assertTrue(ratchet.ocupaLugar());
     }
+
 
 }
