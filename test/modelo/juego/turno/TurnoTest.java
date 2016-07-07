6da02c49a616b3cd638a397f9b5dc126df4eb246
Juan Pablo Capurro <juanpcapurro@gmail.com>
2016-07-07 13:22:17 -0300

    A mitad de hacer los test de Turno

--- test/modelo/juego/turno/TurnoTest.java
+++ test/modelo/juego/turno/TurnoTest.java
@@ -8,6 +8,7 @@ import modelo.juego.jugador.JugadorDecepticons;
 import modelo.tablero.Tablero;
 import modelo.tablero.colocable.robots.autobot.*;
 import modelo.tablero.colocable.robots.decepticon.*;
+import org.junit.Before;
 import org.junit.Test;
 
 public class  TurnoTest {
@@ -17,8 +18,14 @@ public class  TurnoTest {
 	private Jugador jugadorDecepticons = new JugadorDecepticons("Clyde", tablero, new BoneCrusher(), new Frenzy(), new Megatron());
 	private Turno turno = new Turno(jugadorAutobots, jugadorDecepticons);
 
+	@Before
+	public void setUp(){
+		tablero = new Tablero(8, false);
+		jugadorAutobots = new JugadorAutobots("Carlos", tablero, new Bumblebee(), new Optimus(), new Ratchet());
+		jugadorDecepticons = new JugadorDecepticons("Saul", tablero, new BoneCrusher(), new Frenzy(), new Megatron());
+	}
 	@Test
-	public void dummy(){
+	public void losTurnosSePasan(){
 
 	}
 
