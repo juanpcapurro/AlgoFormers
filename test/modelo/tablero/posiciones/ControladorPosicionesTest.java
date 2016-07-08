d148578b96cc43ee7938b32a2568cbdf3437ec34
Juan Pablo Capurro <juanpcapurro@gmail.com>
2016-07-08 00:47:00 -0300

    Haciendo tests para debuguear el controlador de posiciones

---
+++ test/modelo/tablero/posiciones/ControladorPosicionesTest.java
@@ -0,0 +1,29 @@
+package modelo.tablero.posiciones;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ControladorPosicionesTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+    @Test
+    public void iteradorFuncionaAlComienzo(){
+
+    }
+    @Test
+    public void iteradorFuncionaEnLimiteX(){
+
+    }
+    @Test
+    public void iteradorFuncionaEnLimiteY(){
+
+    }
+    @Test
+    public void iteradorFuncionaEnLimiteXY(){
+        
+    }
+}
