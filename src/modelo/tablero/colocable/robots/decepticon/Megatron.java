d9ed45a118d9c11b7c6ff2e1f963ea5d5f4792f8
CharlyTalavera <dontala.-@hotmail.com>
2016-07-09 04:16:14 -0300

    Texto ayuda

--- src/modelo/tablero/colocable/robots/decepticon/Megatron.java
+++ src/modelo/tablero/colocable/robots/decepticon/Megatron.java
@@ -6,8 +6,7 @@ public class Megatron extends Decepticon{
     public Megatron(){
         this.ptosDeVida = new Vida(550);
 		this.contextoModoActual = new ContextoModoAlgoformer();
-        // 10,3,1
-		contextoModoActual.setActual( new ModoHumanoide(ptosDeVida,1000,1000,1000));
+		contextoModoActual.setActual( new ModoHumanoide(ptosDeVida,10,3,1));
         contextoModoActual.setAlternativo(new ModoAlternoAereo(ptosDeVida,55,2,8));
     }
 
